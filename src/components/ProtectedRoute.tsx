2b295308a87f662b0d1d7a7e849872e8436b99e0
Bazaart-dev <reinesdestempsmodernes@gmail.com>
2025-03-27 12:36:54 -0400

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,7 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from './contexts/AuthContext'; // Vous devriez créer ce contexte
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user, loading } = useAuth(); // Récupérez l'état d'authentification depuis un contexte
-  
-  if (loading) {
-    return <div>Chargement...</div>;
-  }
-  
-  return user ? children : <Navigate to="/" replace />;
+  return children;
 };
 
 export default ProtectedRoute;
