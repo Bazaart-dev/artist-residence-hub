1b100f0a7a96de785ea1c71fd2a1b582890479f2
Bazaart-dev <reinesdestempsmodernes@gmail.com>
2025-03-28 14:07:35 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Admin from './pages/Admin';
 import Presentation from './pages/Presentation';
 import Projets from './pages/Projets';
 import Evenements from './pages/Evenements';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 // Admin
 import AdminAuthWrapper from './components/AdminAuthWrapper';
