d819b1151be15c73a0d77f66f36a9312d91d4e52
Bazaart-dev <reinesdestempsmodernes@gmail.com>
2025-03-28 11:52:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SiteProvider } from './contexts/SiteContext';
 import { Toaster } from '@/components/ui/sonner';
-import Index from './pages/Index';
-import Admin from './pages/Admin';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import AdminLayout from './admin/AdminLayout';
 import AdminAuthWrapper from './components/AdminAuthWrapper';
 
 function App() {
@@ -11,15 +13,20 @@ function App() {
     <SiteProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route
-            path="/admin/*"
-            element={
-              <AdminAuthWrapper>
-                <Admin />
-              </AdminAuthWrapper>
-            }
-          />
+          {/* Routes publiques */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+
+          {/* Routes admin protégées */}
+          <Route path="/admin" element={<AdminAuthWrapper><AdminLayout /></AdminAuthWrapper>}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="settings" element={<AdminSettings />} />
+          </Route>
+
+          {/* Route 404 */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
         <Toaster />
       </Router>
