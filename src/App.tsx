fec199549849b72a9b2020ca525dca5ba74f7f5d
Bazaart-dev <reinesdestempsmodernes@gmail.com>
2025-03-28 11:57:47 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
 
           {/* Routes admin protégées */}
           <Route path="/admin" element={<AdminAuthWrapper><AdminLayout /></AdminAuthWrapper>}>
